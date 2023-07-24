2813a09e89ef97feaf58d035b924a9e0f8abb3ed
Peter M. Akasi <118350559+Akasi-M-P@users.noreply.github.com>
2023-07-24 11:59:19 +0000

    initiated player1 and 2 score and dice outcome

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-'use strict';
+const player1Score = document.querySelector('#score--0');
+const player2Score = document.querySelector('#score--1');
+const diceOutcome = document.querySelector('.dice');
+
+player1Score.textContent = 0;
+player2Score.textContent = 0;
+diceOutcome.classList.add('hidden');
